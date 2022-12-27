acb1ef82395893701cf06e154b1c609ce03232fa
Mads Leonardi <mads.leonardi@web.de>
2022-12-27 14:00:14 +0100

    some changes

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,12 +1,27 @@
 import Ingredeant from "./Ingredeant"
+import { useState, useEffect } from 'react'
 
 const Add = ({ data, onDelete, onAdd, onFinish, onUpdate, onUpdateName, onUpdateDescripition }) => {
+    const [textareaheight, setTextareaheight] = useState(1); 
+  
+    const handleChange = (event) => {    
+        //console.log( event.target.rows ) 
+        const height = event.target.scrollHeight; 
+        const rowHeight = 17; 
+        const trows = Math.ceil(height / rowHeight) - 1; 
+        if (trows >= textareaheight || trows < textareaheight) { 
+      
+            setTextareaheight(trows); 
+      
+        }  
+    } 
+
     return (
         <div className='add'>
             <h1 className='Header'>Hinzufügen</h1>
-            <input type="text" placeholder="Name" onChange = {(e) => onUpdateName(e.target.value)}/>
+            <input type="text" placeholder="Name" onChange = {handleChange}/>
             <Ingredeant data={data} onDelete={onDelete} onUpdate={onUpdate}/>
-            <textarea id="addTextarea" name="text" cols="62" rows="5" onChange = {(e) => onUpdateDescripition(e.target.value)}></textarea>
+            <textarea id="addTextarea" name="text" cols="63" rows={textareaheight} onChange = {(e) => handleChange(e)}></textarea>
             <div>
                 <button onClick={onAdd}>Neu Zutat</button>
                 <button onClick={onFinish}>Hinzufügen</button>
